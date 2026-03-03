1533838c8b34ae2f3cbf2287b3061336bd52fc5e
kunalrana15 <kunalranabsr9458@gmail.com>
2026-03-03 08:46:33 +0530

    feat(catalog) create services to add backend catalog api in the frontend

---
+++ src/services/catalogService.ts
@@ -0,0 +1,28 @@
+import api from '../lib/api';
+
+export const catalogService = {
+    async getAllTitles(params?: any) {
+        const response = await api.get("/titles",{ params });
+        return response.data;
+    },
+
+    async getTitleById(id: string) {
+        const response = await api.get(`/title/${id}`);
+        return response.data;
+    },
+
+    async createTitle(data: any) {
+        const response = await api.post("/title",data);
+        return response.data;
+    },
+
+    async updateTitle(id: string,data: any) {
+        const response = await api.put(`/title/${id}`);
+        return response.data
+    },
+
+    async deleteTitle(id: string) {
+        const response = await api.delete(`/title/${id}`);
+        return response.data;
+    }
+}
